ed9c01f9f1cd5d05e1d1aa1b44b62583da8e6d86
PIUphil <ahnkp08@daum.net>
2023-01-17 15:42:52 +0900

    BufferedGraphics

--- etc/BufferedGraphics.cs
+++ etc/BufferedGraphics.cs
@@ -7,6 +7,9 @@ Render - 화면표시
 Dispose - 다 끝난후 리소스 해제
 
 
+OptimizedDoubleBuffer - true이면 컨트롤은 화면에 직접 그려지지 않고 먼저 버퍼에 그려지므로 깜빡임을 줄일 수 있습니다. 
+　　　　　　　　　　　　이 속성을 true로 설정하는 경우 AllPaintingInWmPaint도 true로 설정해야 합니다.
+
 
 // 출처 : https://cw-wd.tistory.com/12
 // https://learn.microsoft.com/ko-kr/dotnet/desktop/winforms/advanced/how-to-manually-render-buffered-graphics
