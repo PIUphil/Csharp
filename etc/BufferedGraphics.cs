56733639147592b061e6b055fbad7357c4c0ddba
PIUphil <ahnkp08@daum.net>
2023-01-17 15:34:34 +0900

    BufferedGraphics

---
+++ etc/BufferedGraphics.cs
@@ -0,0 +1,12 @@
+그래픽버퍼링 BufferedGraphics
+- 많은 양의 그림 뿌릴때 렉 해결
+- 가상의 그래픽 객체 만들어 그곳에 뿌린 후, 다 되면 화면에 표시
+
+Allocate - 가상의 객체
+Render - 화면표시
+Dispose - 다 끝난후 리소스 해제
+
+
+
+// 출처 : https://cw-wd.tistory.com/12
+// https://learn.microsoft.com/ko-kr/dotnet/desktop/winforms/advanced/how-to-manually-render-buffered-graphics
