2a4d340ed19edaae78b6b9d628d7d0ce2df84042
PIUphil <ahnkp08@daum.net>
2023-02-15 11:00:30 +0900

    click & excute

--- etc/execute.cs
+++ etc/execute.cs
@@ -14,7 +14,9 @@ namespace WindowsFormsApp3
         {
             //System.Diagnostics.Process.Start("cmd");
             //System.Diagnostics.Process.Start("calc");
-            System.Diagnostics.Process.Start("notepad");
+            // System.Diagnostics.Process.Start("notepad");
+            System.Diagnostics.Process.Start("note", @"C:\Users\J\Desktop\s.txt"); // C:\windows에 Notepad++프로그램을 바로가기(연결)파일로 note라는 이름으로 저장
+            System.Diagnostics.Process.Start("note", @"C:\Users\J\Desktop\p.txt"); // >> Notepad++로 메모장(txt)파일이 열림
             //System.Diagnostics.Process.Start("mspaint");
         }
     }
